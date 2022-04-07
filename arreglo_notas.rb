f7eb2e6a77634623651c1b1913c5dfc3df88c367
ramon2021tellez <miranda.tellez@gmail.com>
2022-04-06 23:40:50 -0400

    second commit

--- arreglo_notas.rb
+++ arreglo_notas.rb
@@ -1,5 +1,9 @@
-notas = [5,7,1,3,5,8,9,'N.A','N.A',3]
+notas = [5,7,1,3,5,8,9,8,'N.A','N.A',3]
 
-result = notas.map { |x| x != 'N.A' ? x : 2 }
-
-print result
+def promedio(array)
+    result = array.map { |x| x != 'N.A' ? x : 2 }
+    number = result.count
+    prom = result.sum/number
+ 
+end
+puts promedio(notas)
